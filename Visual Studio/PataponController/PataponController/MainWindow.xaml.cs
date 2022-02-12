e95c604c4e8cea8df38319cddae189d8d22987d8
Yaznog <strahl.cedric@gmail.com>
2022-02-12 10:59:49 +0100

    Visual Studio projects

---
+++ Visual Studio/PataponController/PataponController/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.VisualBasic.Devices;
+using Microsoft.VisualBasic;
+
+namespace PataponController
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void walkButton_Click(object sender, RoutedEventArgs e)
+        {
+            //My.Computer.Keyboard.SendKeys("q", true);
+
+            //SendKeys.SendWait("q");
+        }
+
+        private void launchButton_Click(object sender, RoutedEventArgs e)
+        {
+            /*ProcID = Shell("NOTEPAD.EXE", AppWinStyle.NormalFocus);
+            AppActivate(ProcID);*/
+            Process.Start("C:\\Program Files\\PPSSPP\\PPSSPPWindows64.exe");
+        }
+    }
+}
